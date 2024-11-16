9c71712e9ede9a09f166f10a3e63022695b36404
Pradeepkudipudi <pradeepk70220@gmail.com>
2024-11-16 20:18:01 +0530

    renamed

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 
-//solve using for in loop
+// //solve using for in loop
 var car = { brand: "Toyota", model: "corolla",year:2020};
 for (i in car) {
   var x= "" + car[i]; // as it is a number we've to convert it into string ,we used ""
   console.log(`${i} :`,x.toUpperCase());
 };
 
-2
+// 2
 var numbers = [1,2,3,4,5];
 
 
@@ -16,7 +16,7 @@ for (i in numbers){
     console.log(`${numbers[i]}-HI`)
 }
 
-3
+// 3
 
 var fruits = ['apple','banana','cherry','date'];
 
@@ -25,7 +25,7 @@ for(i in fruits){
     console.log(i+"-" + fruits[i])
 }
 
-4
+// 4
 //update city property here
 
 var obj ={
@@ -45,8 +45,8 @@ obj.city = 'san fransisco'
 
 
 
-5     // update  model & year here
-var car = {
+// 5     // update  model & year here
+ var car = {
     brand : 'toyota',
     model :'camry',
     year : 2020
@@ -55,7 +55,7 @@ car.model = 'corolla'
 
 console.log(car)
 
-6 // add an ingredient here
+// 6 // add an ingredient here
 
 var recipe = {
 
